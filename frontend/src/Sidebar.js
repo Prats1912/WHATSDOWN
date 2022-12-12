c62c04a819e9e1699016d09661c48d69b777394c
ppatel <ppatel@mediaocean.com>
2022-12-13 00:29:04 +0530

    frontend setting basic UI structure with placeholders

---
+++ frontend/src/Sidebar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Sidebar.css";
+import { IconButton, Avatar } from "@material-ui/core";
+import { SearchOutlined,ChatIcon, DonutLargeIcon, MoreVertIcon} from "@mui/icons-material";
+import SidebarChat from "./SidebarChat";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar src="" />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input placeholder="Search or start new chat" type="text" />
+        </div>
+      </div>
+      <div className="sidebar__chats">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ frontend/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA8CTYI0NVE6ZRxG1lg1DodGyxg7FbTsuM",
+    authDomain: "chatapp-b6e0e.firebaseapp.com",
+    projectId: "chatapp-b6e0e",
+    storageBucket: "chatapp-b6e0e.appspot.com",
+    messagingSenderId: "874246074046",
+    appId: "1:874246074046:web:ef435a58c7ab1bb994f8ef"
+  };
